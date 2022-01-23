ecb9f4b615d037dfdc54f83a79fe9f7ddce7f0d1
elis-11 <eliza.arzanukaeva@gmail.com>
2022-01-23 21:23:03 +0100

    create some files

---
+++ extra/images/getpics.sh
@@ -0,0 +1,17 @@
+
+------create-people-cards-------
+
+elis@11:~/webs/hooks/images$:
+
+#!/bin/bash
+ 
+MAX=20
+if [[ ! -z "$1" ]]; then
+        MAX=$1
+fi
+ 
+for i in $(seq -f "%03g" 1 $MAX); do
+    FILE=person${i}.jpg
+    curl 'https://thispersondoesnotexist.com/image' -H 'authority: thispersondoesnotexist.com' -H 'pragma: no-cache' -H 'cache-control: no-cache' -H 'upgrade-insecure-requests: 1' -H 'user-agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.80 Safari/537.36' -H 'accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3' -H 'referer: https://thispersondoesnotexist.com/' -H 'accept-encoding: gzip, deflate, br' -H 'accept-language: en-US,en;q=0.9' --compressed -o $FILE
+    sleep 1
+done
--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import './App.scss'
+// import elis from '../public/images/cards/elis.png'
+import elis from './cards/elis.png'
 
 const App = () => {
+
   return ( 
   <div className="App">
+<img src={elis} alt="logo" className="logo" />
 tralala
   </div>
   )
