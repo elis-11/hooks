8e45727119bae3d0f6ecdbb74ea1a41893a5bd45
elis-11 <eliza.arzanukaeva@gmail.com>
2022-01-30 17:24:01 +0100

    random()

--- src/App.js
+++ src/App.js
@@ -2,17 +2,26 @@ import "./App.scss";
 // import { Navbar } from "./components/Navbar";
 
 const App = () => {
-
-const name = 'Dave'
-const name1 = 'John'
+//--1
+// const name = 'Dave'
+// const name1 = 'John'
+// --2
+const handleNameChange = () => {
+  const names = ['Rob', 'John', 'Dave']
+  const concret = Math.floor(Math.random() *3)
+  return names[concret]
+}
 
   return (
     <div className="App">
       {/* <Navbar /> */}
-      Hi <p>Elis</p>
+      {/* --1 */}
+      {/* Hi <p>Elis</p>
        <p>Hello {name1}!</p>
        <p>Hello {name}!</p>
-      <p>Hello World!</p>
+      <p>Hello World!</p> */}
+      {/* --2 */}
+      Hello {handleNameChange()}
     </div>
   );
 };
