e5f377e3e9b317908407ddbf6a59df6e14f14084
elis-11 <eliza.arzanukaeva@gmail.com>
2022-01-30 21:53:33 +0100

    UseState & React-Icons

--- public/javascript.md
+++ public/javascript.md
@@ -83,4 +83,46 @@ export const Content = () => {
   );
 };
              || useState
-             
+
+ import { useState } from "react";
+
+export const Content = () => {
+  //--style
+  const contentStyle = { color: "white" };
+
+const [name, setName] = useState('')
+const [count, setCount] = useState(0)
+
+  //--1
+  const handleNameChange = () => {
+    const names = ["Rob", "John", "Dave", "Peter", "Luis"];
+    const concret = Math.floor(Math.random() * 5);
+    setName(names[concret]);
+  };
+  //--2
+  const handleClick = () => {
+      setCount(count + 1)
+      setCount(count + 1)
+    console.log(count);
+  };
+  //--3
+  const handleClick2 = () => {
+    console.log(count);
+  };
+
+  return (
+    //--style
+    <main style={contentStyle}>
+      {/* --1 --20 */}
+      <p onDoubleClick={handleClick}>
+          Hello {name}!
+      </p>
+      {/*  --44 */}
+      <button onClick={handleNameChange}>Change Name</button>
+      {/* --2 --44 */}
+      <button onClick={handleClick}>Click it</button>
+      {/* --3 --48 */}
+      <button onClick={handleClick2}>Click it</button>
+    </main>
+  );
+};
--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,42 +1,47 @@
 import { useState } from "react";
+import { FaTrashAlt} from 'react-icons/fa'
 
 export const Content = () => {
   //--style
   const contentStyle = { color: "white" };
 
-const [name, setName] = useState('')
-const [count, setCount] = useState(0)
-
-  //--1
-  const handleNameChange = () => {
-    const names = ["Rob", "John", "Dave", "Peter", "Luis"];
-    const concret = Math.floor(Math.random() * 5);
-    setName(names[concret]);
-  };
-  //--2
-  const handleClick = () => {
-      setCount(count + 1)
-      setCount(count + 1)
-    console.log(count);
-  };
-  //--3
-  const handleClick2 = () => {
-    console.log(count);
-  };
+  const [items, setItems] = useState([
+    {
+      id: 1,
+      checked: false,
+      item: "tralala",
+    },
+    {
+      id: 2,
+      checked: false,
+      item: "item 2",
+    },
+    {
+      id: 3,
+      checked: false,
+      item: "item 3",
+    },
+  ]);
 
   return (
     //--style
     <main style={contentStyle}>
-      {/* --1 --20 */}
-      <p onDoubleClick={handleClick}>
-          Hello {name}!
-      </p>
-      {/*  --44 */}
-      <button onClick={handleNameChange}>Change Name</button>
-      {/* --2 --44 */}
-      <button onClick={handleClick}>Click it</button>
-      {/* --3 --48 */}
-      <button onClick={handleClick2}>Click it</button>
+        <ul>
+            {items.map((item) => (
+                <li className="item" key={item.id}>
+                    <input 
+                    type="checkbox"
+                    checked={item.checked}
+                    />
+                    <label>{item.item}</label>
+                    {/* <button>Delete</button> */}
+                    <FaTrashAlt 
+                    role="button" 
+                    tabIndex="0" 
+                    />
+                </li>
+            ))}
+        </ul>
     </main>
   );
 };
