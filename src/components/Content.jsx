c49791d49b0654aa877794d8244bedfeeb1a7ac2
elis-11 <eliza.arzanukaeva@gmail.com>
2022-01-31 20:27:13 +0100

    1:04nLists & Keys

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { FaTrashAlt} from 'react-icons/fa'
+import { FaTrashAlt } from "react-icons/fa";
 
 export const Content = () => {
   //--style
@@ -23,25 +23,30 @@ export const Content = () => {
     },
   ]);
 
+  const handleCheck = (id) => {
+    const listItems = items.map((item) =>
+      item.id === id ? { ...item, checked: !item.checked } : item
+    );
+    setItems(listItems);
+  };
+
   return (
     //--style
     <main style={contentStyle}>
-        <ul>
-            {items.map((item) => (
-                <li className="item" key={item.id}>
-                    <input 
-                    type="checkbox"
-                    checked={item.checked}
-                    />
-                    <label>{item.item}</label>
-                    {/* <button>Delete</button> */}
-                    <FaTrashAlt 
-                    role="button" 
-                    tabIndex="0" 
-                    />
-                </li>
-            ))}
-        </ul>
+      <ul>
+        {items.map((item) => (
+          <li className="item" key={item.id}>
+            <input
+              type="checkbox"
+              onChange={() => handleCheck(item.id)}
+              checked={item.checked}
+            />
+            <label>{item.item}</label>
+            {/* <button>Delete</button> */}
+            <FaTrashAlt role="button" tabIndex="0" />
+          </li>
+        ))}
+      </ul>
     </main>
   );
 };
