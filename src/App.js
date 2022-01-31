073e38e8e18459dd26ff6ab607164e9a4e028b87
elis-11 <eliza.arzanukaeva@gmail.com>
2022-01-31 17:50:01 +0100

    styling doc

--- src/App.js
+++ src/App.js
@@ -2,23 +2,13 @@ import "./App.scss";
 import { Content } from "./components/Content";
 import { Foother } from "./components/Foother";
 import { Header } from "./components/Header";
-// import { Navbar } from "./components/Navbar";
 
 const App = () => {
-  //--1
   // const name = 'Dave'
-  // const name1 = 'John'
-  // --2
-
   return (
     <div className="App">
-      {/* <Navbar /> */}
-      {/* --1 */}
-      {/* Hi <p>Elis</p>
-       <p>Hello {name1}!</p>
-       <p>Hello {name}!</p>
-      <p>Hello World!</p> */}
-      {/* --2 */}
+       {/* <p>Hello {name}!</p> */}
+      {/* <p>Hello World!</p> */}
       <Header />
       <Content />
       <Foother />
@@ -26,3 +16,4 @@ const App = () => {
   );
 };
 export default App;
+// 1:17
