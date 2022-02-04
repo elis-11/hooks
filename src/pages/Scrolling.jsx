6a4c58f551d1238f439eda78c784c009feb046dd
elis-11 <eliza.arzanukaeva@gmail.com>
2022-02-04 17:33:35 +0100

    Create Scrolling :(

---
+++ src/pages/Scrolling.jsx
@@ -0,0 +1,42 @@
+import React, { useRef } from "react";
+import "../App.scss";
+
+export const Scrolling = () => {
+  const paraRef = useRef(null);
+  const clickHandler = () => {
+    paraRef.current &&
+      paraRef.current.scrollIntoView({ behavior: "smooth", block: "start" });
+  };
+  return (
+    <div className="Scrolling">
+      <button onClick={clickHandler}>Scroll to Next para</button>
+      <p className="section">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Unde eum
+        repudiandae ut tempore laudantium, provident labore doloremque sit
+        magnam, minima temporibus explicabo voluptatibus cupiditate a culpa
+        reprehenderit magni, qui aspernatur! Laborum a iure doloribus, officia
+        earum asperiores ut, hic voluptates libero sed consequuntur facere
+        itaque natus quisquam! Numquam explicabo sint saepe porro, qui quibusdam
+        nam eum minima quasi temporibus. Non?
+      </p>
+      <p className="section" ref={paraRef}>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Unde eum
+        repudiandae ut tempore laudantium, provident labore doloremque sit
+        magnam, minima temporibus explicabo voluptatibus cupiditate a culpa
+        reprehenderit magni, qui aspernatur! Laborum a iure doloribus, officia
+        earum asperiores ut, hic voluptates libero sed consequuntur facere
+        itaque natus quisquam! Numquam explicabo sint saepe porro, qui quibusdam
+        nam eum minima quasi temporibus. Non?
+      </p>
+      <p className="section">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Unde eum
+        repudiandae ut tempore laudantium, provident labore doloremque sit
+        magnam, minima temporibus explicabo voluptatibus cupiditate a culpa
+        reprehenderit magni, qui aspernatur! Laborum a iure doloribus, officia
+        earum asperiores ut, hic voluptates libero sed consequuntur facere
+        itaque natus quisquam! Numquam explicabo sint saepe porro, qui quibusdam
+        nam eum minima quasi temporibus. Non?
+      </p>
+    </div>
+  );
+};
