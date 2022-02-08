c51fbddb10fd2afb590b9aa8f02f4167be617242
elis-11 <eliza.arzanukaeva@gmail.com>
2022-02-08 17:48:01 +0100

    Search

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,39 +1,44 @@
-import React, { useRef } from "react"
-import { useLocation, useNavigate } from "react-router-dom"
+import React, { useRef } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import "../styles/Search.scss";
 
 function useQuery() {
   // Use the URLSearchParams API to extract the query parameters
   // useLocation().search will have the query parameters eg: ?foo=bar&a=b
-  return new URLSearchParams(useLocation().search)
+  return new URLSearchParams(useLocation().search);
 }
 
 const Search = () => {
-  const query = useQuery()
+  const query = useQuery();
 
-  const term = query.get("term")
+  const term = query.get("term");
 
-  const inputRef = useRef(null)
-  const navigate = useNavigate()
+  const inputRef = useRef(null);
+  const navigate = useNavigate();
 
-  const formSubmitHandler = e => {
+  const formSubmitHandler = (e) => {
     //prevent the default form submission
-    e.preventDefault()
+    e.preventDefault();
 
     //extract search term using refs.
-    const searchValue = inputRef.current.value
-    navigate(`?term=${searchValue}`)
-  }
+    const searchValue = inputRef.current.value;
+    navigate(`?term=${searchValue}`);
+  };
 
   return (
-    <div>
+    <div className="search">
       <form action="" onSubmit={formSubmitHandler}>
-        <input type="text" name="term" ref={inputRef} />
-        <input type="submit" value="Search" />
+        <div className="container">
+        <input className="first" type="text" name="term" ref={inputRef} />
+        <input className="second" type="submit" value="Search" />
+        </div>
+        <div className="container">
         {/* Display the search term if it is present */}
         {term && <h2>Results for '{term}'</h2>}
+        </div>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default Search
+export default Search;
