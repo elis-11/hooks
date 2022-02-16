f720cc744b332220951802138768bcbbcc70f17a
elis-11 <eliza.arzanukaeva@gmail.com>
2022-02-16 20:08:54 +0100

    props

--- src/components/todo/Header.jsx
+++ src/components/todo/Header.jsx
@@ -1,10 +1,12 @@
 import "../../styles/Todo.scss";
 
-
-export const Header = () => {
+export const Header = ({ title }) => {
   return (
     <header style={{ color: "white" }}>
-      <h2>Groceries List</h2>
+      <h2>{title}</h2>
     </header>
   );
+};
+Header.defaultProps = {
+  title: "Grocery",
 };
--- src/components/todo/Todo.jsx
+++ src/components/todo/Todo.jsx
@@ -1,15 +1,14 @@
-import {Header} from './Header'
-import {Content} from './Content'
-import {Footer} from './Footer'
+import { Header } from "./Header";
+import { Content } from "./Content";
+import { Footer } from "./Footer";
 import "../../styles/Todo.scss";
-
 
 export const Todo = () => {
   return (
     <div className="Todo">
-<Header />
-<Content />
-<Footer />
+      <Header title="Grocery List" />
+      <Content />
+      <Footer />
     </div>
-  )
-}
+  );
+};
