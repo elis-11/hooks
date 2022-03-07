f24e869600af128d21d0ca7154b9a9082f1c740a
elis-11 <eliza.arzanukaeva@gmail.com>
2022-03-07 14:59:02 +0100

    LocalStorage changes

--- src/components/todo/Todo.jsx
+++ src/components/todo/Todo.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Header } from "./Header";
 import { Content } from "./Content";
 import { Footer } from "./Footer";
@@ -7,31 +7,32 @@ import { SearchItem } from "./SearchItem";
 import "./Todo.scss";
 
 export const Todo = () => {
-  const [items, setItems] = useState(JSON.parse(localStorage.getItem("Todo")));
+  const [items, setItems] = useState(
+    JSON.parse(localStorage.getItem("Todo")) || []
+  );
   const [newItem, setNewItem] = useState("");
   const [search, setSearch] = useState("");
 
-  const setAndSaveItems = (newItems) => {
-    setItems(newItems);
-    localStorage.setItem("Todo", JSON.stringify(newItems));
-  };
+  useEffect(() => {
+    localStorage.setItem("Todo", JSON.stringify(items));
+  }, [items]);
 
   const addItem = (list) => {
     const id = items.length ? items[items.length - 1].id + 1 : 1;
     const myNewItem = { id, checked: false, list };
     const listItems = [...items, myNewItem];
-    setAndSaveItems(listItems);
+    setItems(listItems);
   };
 
   const handleCheck = (id) => {
     const listItems = items.map((item) =>
       item.id === id ? { ...item, checked: !item.checked } : item
     );
-    setAndSaveItems(listItems);
+    setItems(listItems);
   };
   const handleDelete = (id) => {
     const listItems = items.filter((item) => item.id !== id);
-    setAndSaveItems(listItems);
+    setItems(listItems);
   };
 
   const handleSubmit = (e) => {
