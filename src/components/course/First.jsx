640e2565eda8dbb4e9370df4531a570aa2d1a339
elis-11 <eliza.arzanukaeva@gmail.com>
2022-03-25 20:10:15 +0100

    Polished comment.

--- src/components/course/First.jsx
+++ src/components/course/First.jsx
@@ -1,7 +1,27 @@
-import React from 'react'
+import React from "react";
+// import { listItems } from "./apis/ListItem";
 
 export const First = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("You clicked submit.");
+  };
+
+  // const numbers=props.numbers
+  const numbers = [1, 2, 3, 4, 5];
+  const listItems = numbers.map((number) => <li key={number.toString()}>{number}</li>);
+  console.log(listItems);
   return (
-    <div>First</div>
-  )
-}
+    <div className="First">
+      <h2>First</h2>
+
+      <h3 className="Date">Is is {new Date().toLocaleTimeString()}.</h3>
+
+      <form onSubmit={handleSubmit}>
+        <button type="submit">Submit</button>
+      </form>
+
+      <ul>{listItems}</ul>
+    </div>
+  );
+};
