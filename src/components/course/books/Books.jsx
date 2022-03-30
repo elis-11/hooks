86e413a7a9439b6efcde42063cc1b36e2a9fef26
elis-11 <eliza.arzanukaeva@gmail.com>
2022-03-30 18:52:56 +0200

    Add functionality

--- src/components/course/books/Books.jsx
+++ src/components/course/books/Books.jsx
@@ -1,10 +1,31 @@
-import React from "react";
+import { useState } from "react";
+import { FaEdit } from "react-icons/fa";
+import { FaTrashAlt } from "react-icons/fa";
+
 import "./Books.scss";
-// import { useState } from "react";
 
 export const Books = () => {
-  //   const [books, setBooks] = useState([]);
-  //   const [book, setBook] = useState("");
+  const [books, setBooks] = useState([]);
+  const [book, setBook] = useState("");
+
+  const handleInputChange = (e) => {
+    setBook(e.target.value);
+  };
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+
+    if (book !== "") {
+      setBooks([
+        ...books,
+        {
+          id: books.length + 1,
+          text: book.trim(),
+        },
+      ]);
+    }
+    setBook("");
+  };
 
   return (
     <div className="Books">
@@ -12,20 +33,30 @@ export const Books = () => {
         <h2>Books List</h2>
       </header>
       <div className="container">
-        <div>
-          <input name="book" type="text" placeholder="Create a new Book" />
-        </div>
+        <form onSubmit={handleFormSubmit}>
+          <input
+            name="book"
+            type="text"
+            placeholder="Create a new Book"
+            value={book}
+            onChange={handleInputChange}
+          />
+        </form>
         <div className="book-list">
-          {/* {books.map((book) => ( */}
-          {/* <div key={book.id} className="book">
-            <div>{book.title}</div>
-            <div>{book.author}</div>
-            <div className="icons">icons</div>
-        </div> */}
-          {/* ))} */}
+          {books.map((book) => (
+            <div key={book.id} className="book">
+              {book.text}
+              <div>{book.title}</div>
+              <div>{book.author}</div>
+              <div className="icons">
+                <FaEdit className="icon" role="button" tabIndex="0" />
+                <FaTrashAlt className="icon" role="button" tabIndex="0" />
+              </div>
+            </div>
+          ))}
         </div>
       </div>
-      <footer> Copyright &copy; 2022</footer>
+      <footer>Copyright &copy; 2022</footer>
     </div>
   );
 };
