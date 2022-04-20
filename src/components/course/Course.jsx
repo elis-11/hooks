72626d3c5ab63cb386fda2151bffb1483fb346e2
elis-11 <eliza.arzanukaeva@gmail.com>
2022-04-20 20:16:23 +0200

    Added new functionality.

--- src/components/course/Course.jsx
+++ src/components/course/Course.jsx
@@ -1,34 +1,30 @@
 import React from "react";
-import { Routes, Link, Route } from "react-router-dom";
+import { Routes, NavLink, Route } from "react-router-dom";
 import { First } from "./First";
 import { Color } from "./color/Color";
-import './Course.scss'
 import { Apis } from "./apis/Apis";
 import { Java } from './java/Java';
 import { Todos } from './todos/Todos';
 import { Books } from "./books/Books";
 import { Name } from "./name/Name";
+import './Course.scss'
 
 export const Course = () => {
   return (
     <div className="Course">
-        <div className="course-comp">
-      <ul>
-        <li>
-          <Link to="">First</Link>
-          <Link to="color">Color</Link>
-          <Link to="books">Books</Link>
-          <Link to="apis">Apis</Link>
-          <Link to="java">Java</Link>
-          <Link to="todos">Todos</Link>
-          <Link to="name">Name</Link>
-        </li>
-      </ul>
+        <div className="links">
+          <NavLink to="" end>Books</NavLink>
+          <NavLink to="first" >First</NavLink>
+          <NavLink to="color">Color</NavLink>
+          <NavLink to="apis">Apis</NavLink>
+          <NavLink to="java">Java</NavLink>
+          <NavLink to="todos">Todos</NavLink>
+          <NavLink to="name">Name</NavLink>
         </div>
       <Routes>
-        <Route path="/" element={<First />} />
+        <Route path="/" element={<Books />} />
+        <Route path="first" element={<First />} />
         <Route path="color" element={<Color />} />
-        <Route path="books" element={<Books />} />
         <Route path="apis" element={<Apis />} />
         <Route path="todos" element={<Todos />} />
         <Route path="java" element={<Java />} />
